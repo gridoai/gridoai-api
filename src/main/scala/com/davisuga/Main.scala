fbf60a63be1539aa1fff1edf90f5a13166b35232
davisuga <daviciencia1@gmail.com>
2023-05-23 10:00:58 -0300

    Basic scala function for GCP

---
+++ src/main/scala/com/davisuga/Main.scala
@@ -0,0 +1,14 @@
+package com.davisuga
+
+import com.google.cloud.functions.{HttpFunction, HttpRequest, HttpResponse}
+
+// [START functions_api_get]
+class ScalaHttpFunction extends HttpFunction {
+  override def service(
+      httpRequest: HttpRequest,
+      httpResponse: HttpResponse
+  ): Unit = {
+    httpResponse.getWriter.write("Hello World!")
+  }
+}
+// [END functions_api_get]
