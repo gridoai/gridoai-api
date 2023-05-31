6a80467eb3e4ad2bd6ddfe594cecbdea8d6b2f44
davisuga <daviciencia1@gmail.com>
2023-05-31 15:58:04 -0300

    Using ogm

--- src/main/scala/com/programandonocosmos/Domain.scala
+++ src/main/scala/com/programandonocosmos/Domain.scala
@@ -1,11 +1,44 @@
 package com.programandonocosmos.domain
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-import io.circe.syntax._
+import org.neo4j.ogm.annotation.EndNode
+import org.neo4j.ogm.annotation.GeneratedValue
+import org.neo4j.ogm.annotation.Id
+import org.neo4j.ogm.annotation.NodeEntity
+import org.neo4j.ogm.annotation.Property
+import org.neo4j.ogm.annotation.Relationship
+import org.neo4j.ogm.annotation.Relationship.Direction
+import org.neo4j.ogm.annotation.RelationshipEntity
+import org.neo4j.ogm.annotation.StartNode
+import org.neo4j.ogm.session.Session
+
+import java.util.UUID
 
 type ID = String
+type UID = UUID
 
-case class Folder(id: ID, name: String, url: String)
-case class Page(id: ID, name: String, content: String, url: String)
-case class Contains(folderId: ID, pageId: ID)
+@NodeEntity
+case class Document(
+    @Id @GeneratedValue
+    id: ID,
+    @Property(name = "uid")
+    uid: UID,
+    @Property(name = "name")
+    name: String,
+    @Property(name = "content")
+    content: String,
+    @Property(name = "url")
+    url: String,
+    @Property(name = "numberOfWords")
+    numberOfWords: Int,
+    @Relationship(`type` = "MENTIONS", direction = Direction.OUTGOING)
+    mentions: List[Mentions] = List.empty[Mentions]
+)
+
+@RelationshipEntity(`type` = "MENTIONS")
+case class Mentions(
+    @Id @GeneratedValue
+    relationshipId: Long,
+    @StartNode
+    from: Document,
+    @EndNode
+    to: Document
+)
