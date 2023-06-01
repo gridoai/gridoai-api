b799de4f02439c47a398c56cc66c76157cf9d81c
davisuga <daviciencia1@gmail.com>
2023-06-01 15:06:05 -0300

    Fixed namings and signatures of DocDB trait

--- src/main/scala/com/programandonocosmos/models/DocDB.scala
+++ src/main/scala/com/programandonocosmos/models/DocDB.scala
@@ -1,16 +1,12 @@
 package com.programandonocosmos.models
 
-import com.programandonocosmos.domain.Contains
-import com.programandonocosmos.domain.Folder
-import com.programandonocosmos.domain.ID
-import com.programandonocosmos.domain.Page
+import com.programandonocosmos.domain.Document
+import com.programandonocosmos.domain.Mentions
+import com.programandonocosmos.domain.UID
 
 trait DocDB[F[_]]:
-  def addPage(page: Page): F[Unit]
-  def addFolder(folder: Folder): F[Unit]
-  def addContains(contains: Contains): F[Unit]
-  def getPagesById(ids: List[ID]): F[List[Page]]
-  def linkPageToFolder(relation: Contains): F[Unit]
-  def addPages(pages: List[Page]): F[Unit]
-  def addFolders(folders: List[Folder]): F[Unit]
-  def addContainsRelations(contains: List[(Page, Folder)]): F[Unit]
+  def addDocument(doc: Document): F[Unit]
+  def addMentions(mentions: Mentions): F[Unit]
+  def getDocumentsByIds(ids: List[UID]): F[List[Document]]
+  def getDocumentById(id: UID): F[Document]
+  def getDocumentMentions(id: UID): F[List[Document]]
