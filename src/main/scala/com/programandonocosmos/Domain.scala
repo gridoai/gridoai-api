7be1e06192e63da6d81dafe9ea2cd458d62b42fe
davisuga <daviciencia1@gmail.com>
2023-06-01 15:05:14 -0300

    Removed ogm code

--- src/main/scala/com/programandonocosmos/Domain.scala
+++ src/main/scala/com/programandonocosmos/Domain.scala
@@ -1,44 +1,23 @@
 package com.programandonocosmos.domain
-import org.neo4j.ogm.annotation.EndNode
-import org.neo4j.ogm.annotation.GeneratedValue
-import org.neo4j.ogm.annotation.Id
-import org.neo4j.ogm.annotation.NodeEntity
-import org.neo4j.ogm.annotation.Property
-import org.neo4j.ogm.annotation.Relationship
-import org.neo4j.ogm.annotation.Relationship.Direction
-import org.neo4j.ogm.annotation.RelationshipEntity
-import org.neo4j.ogm.annotation.StartNode
-import org.neo4j.ogm.session.Session
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
 import java.util.UUID
 
-type ID = String
 type UID = UUID
 
-@NodeEntity
 case class Document(
-    @Id @GeneratedValue
-    id: ID,
-    @Property(name = "uid")
     uid: UID,
-    @Property(name = "name")
     name: String,
-    @Property(name = "content")
     content: String,
-    @Property(name = "url")
     url: String,
-    @Property(name = "numberOfWords")
-    numberOfWords: Int,
-    @Relationship(`type` = "MENTIONS", direction = Direction.OUTGOING)
-    mentions: List[Mentions] = List.empty[Mentions]
+    numberOfWords: Int
 )
 
-@RelationshipEntity(`type` = "MENTIONS")
 case class Mentions(
-    @Id @GeneratedValue
     relationshipId: Long,
-    @StartNode
-    from: Document,
-    @EndNode
-    to: Document
+    from: UID,
+    to: UID
 )
