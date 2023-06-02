907f5d08053e731a9ea2b01a899a7dc43036cf75
davisuga <daviciencia1@gmail.com>
2023-06-02 17:12:16 -0300

    Added tracing utilities

---
+++ src/main/scala/com/programandonocosmos/utils/Trace.scala
@@ -0,0 +1,13 @@
+package com.programandonocosmos.utils
+
+import cats.effect.IO
+
+extension [T](a: T)
+  def trace =
+    println(a)
+    a
+
+extension [T](a: IO[T])
+  def trace(msg: String = "") =
+    a.attempt.map(println)
+    a
