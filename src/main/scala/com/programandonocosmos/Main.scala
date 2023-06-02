692eb4fdca5e530ad033fb2b309833359f928657
davisuga <daviciencia1@gmail.com>
2023-06-02 17:17:32 -0300

    Simple http4s setup

--- src/main/scala/com/programandonocosmos/Main.scala
+++ src/main/scala/com/programandonocosmos/Main.scala
@@ -1,14 +1,53 @@
 package com.programandonocosmos
+import cats.effect.IO
+import cats.effect.IOApp
+import cats.effect.*
+import cats.effect.unsafe.implicits.global
+import cats.implicits.*
+import com.comcast.ip4s.ipv4
+import com.comcast.ip4s.port
+import com.google.cloud.functions.HttpFunction
+import com.google.cloud.functions.HttpRequest
+import com.google.cloud.functions.HttpResponse
+import com.programandonocosmos.adapters.Neo4jAsync
+import com.programandonocosmos.domain.Document
+import com.programandonocosmos.domain.Mentions
+import com.programandonocosmos.endpoints.*
+import com.programandonocosmos.models.DocDB
+import com.programandonocosmos.models.Neo4j
+import de.killaitis.http4s.*
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import io.circe.syntax.*
+import org.http4s.ember.server.EmberServerBuilder
 
-import com.google.cloud.functions.{HttpFunction, HttpRequest, HttpResponse}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.jdk.CollectionConverters.*
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
 
-// [START functions_api_get]
 class ScalaHttpFunction extends HttpFunction {
-  override def service(
-      httpRequest: HttpRequest,
-      httpResponse: HttpResponse
-  ): Unit = {
-    httpResponse.getWriter.write("Hello World!")
-  }
+  def service(request: HttpRequest, response: HttpResponse) =
+    Neo4jAsync.resourceWithCredentials.use { runner =>
+      given docDb: DocDB[IO] = Neo4j(runner)
+      IO.pure(Http4sCloudFunction(HttpApp).service(request, response))
+    }
 }
-// [END functions_api_get]
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    Neo4jAsync.resourceWithCredentials.use { runner =>
+      given docDb: DocDB[IO] = Neo4j(runner)
+      EmberServerBuilder
+        .default[IO]
+        .withHost(ipv4"0.0.0.0")
+        .withPort(port"8080")
+        .withHttpApp(HttpApp)
+        .build
+        .use(_ => IO.never)
+        .as(ExitCode.Success)
+
+    }
+}
