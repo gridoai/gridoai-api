9b8dbac17cc6c6e2c528de9fec25d7c3b07bb080
davisuga <daviciencia1@gmail.com>
2023-06-03 13:48:00 -0300

    Function to check if host is reachable

--- src/main/scala/com/programandonocosmos/adapters/HttpClient.scala
+++ src/main/scala/com/programandonocosmos/adapters/HttpClient.scala
@@ -4,6 +4,10 @@ import sttp.client3._
 import sttp.client3.httpclient.cats.HttpClientCatsBackend
 import sttp.model.StatusCode
 
+import java.net.InetAddress
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
 val catsBackend =
   ((HttpClientCatsBackend
     .resource[IO]()
@@ -13,7 +17,13 @@ def sendRequest[T] = (r: Request[T, Any]) => catsBackend.flatMap(r.send)
 
 extension [T](r: Request[T, Any]) def sendReq() = sendRequest(r)
 
+def isHostReachable(
+    host: String
+) =
+  InetAddress.getByName(host).isReachable(1000)
+
 class HttpClient(endpoint: String):
+
   def get(path: String) =
     basicRequest.get(uri"${endpoint + path}")
 
