80a21501f026e78c51e4045d2fc41609393d4524
davisuga <daviciencia1@gmail.com>
2023-06-03 15:48:33 -0300

    Checking if host is reachable

--- src/main/scala/com/programandonocosmos/adapters/contextHandler/Http.scala
+++ src/main/scala/com/programandonocosmos/adapters/contextHandler/Http.scala
@@ -11,8 +11,9 @@ import sttp.client3._
 import sttp.client3._
 import sttp.client3.httpclient.cats.HttpClientCatsBackend
 import sttp.model.StatusCode
+import com.programandonocosmos.mock.mockedDoc
 
-val endpoint = sys.env.getOrElse(
+val contextHandlerEndpoint = sys.env.getOrElse(
   "CONTEXT_HANDLER_ENDPOINT",
   "http://127.0.0.1:8000"
 )
@@ -21,9 +22,26 @@ case class MessageResponse[T](message: T)
 
 type DocResponseItem = (String, Float)
 type DocResponse = List[DocResponseItem]
+trait DocumentApiClient:
+  def write(
+      uid: String,
+      text: String
+  ): IO[Either[String | io.circe.Error, MessageResponse[List[Float]]]]
 
-object DocumentApiClient:
-  val Http = HttpClient(endpoint)
+  def delete(uid: String): IO[StatusCode]
+
+  def neardocs(
+      text: String
+  ): IO[Either[String | io.circe.Error, MessageResponse[DocResponse]]]
+
+object DocumentApiClientHttp extends DocumentApiClient:
+  val Http = HttpClient(contextHandlerEndpoint)
+
+  if (isHostReachable(contextHandlerEndpoint)) != true then
+    throw new Exception(
+      "Context Handler is not reachable"
+    )
+
   def write(
       uid: String,
       text: String
@@ -46,3 +64,29 @@ object DocumentApiClient:
       .sendReq()
       .trace("Response: ")
       .map(_.body.trace.flatMap(decode[MessageResponse[DocResponse]]))
+
+object MockDocumentApiClient extends DocumentApiClient:
+  val mockResponse: MessageResponse[List[Float]] = MessageResponse(
+    List(1.0f, 2.0f, 3.0f)
+  )
+  val mockDocResponse: MessageResponse[DocResponse] = MessageResponse(
+    List((mockedDoc.uid.toString(), 0.5f), (mockedDoc.uid.toString(), 0.3f))
+  )
+
+  override def write(
+      uid: String,
+      text: String
+  ): IO[Either[String | io.circe.Error, MessageResponse[List[Float]]]] =
+    IO.pure(Right(mockResponse))
+
+  override def delete(uid: String): IO[StatusCode] =
+    IO.pure(StatusCode.Ok)
+
+  override def neardocs(
+      text: String
+  ): IO[Either[String | io.circe.Error, MessageResponse[DocResponse]]] =
+    IO.pure(Right(mockDocResponse))
+
+val DocumentApiClient =
+  if (sys.env.get("ENV") == Some("TEST")) then MockDocumentApiClient
+  else DocumentApiClientHttp
