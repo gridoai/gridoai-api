47a09211f15bc8304ab3aaee47cb08638f12edb3
davisuga <daviciencia1@gmail.com>
2023-06-06 21:46:21 -0300

    Writing doc to vector db

--- src/main/scala/com/programandonocosmos/services/doc/Docs.scala
+++ src/main/scala/com/programandonocosmos/services/doc/Docs.scala
@@ -1,40 +1,45 @@
 package com.programandonocosmos.services.doc
 
 import cats.effect.IO
-import com.programandonocosmos.adapters.contextHandler.DocumentApiClient
+import com.programandonocosmos.adapters.contextHandler.{
+  DocumentApiClient,
+  MessageResponse
+}
 import com.programandonocosmos.domain.Document
 import com.programandonocosmos.domain.UID
 import com.programandonocosmos.models.DocDB
 
 import java.util.UUID
-
 import util.chaining.scalaUtilChainingOps
+import cats.syntax.parallel.*
 
 def searchDoc(
     x: String
 )(implicit db: DocDB[IO]): IO[Either[String, List[Document]]] = {
   println("Searching for: " + x)
-  DocumentApiClient.neardocs(x).flatMap { responseEither =>
-    responseEither match {
-      case Right(response) =>
-        val ids = response.message.map(_._1.pipe(UUID.fromString))
-        println(f"Searching ids: $ids")
-        db.getDocumentsByIds(ids).attempt.map {
-          case Right(docs) => Right(docs)
-          case Left(err)   => Left(err.getMessage) // Map the error to a string
-        }
+  DocumentApiClient.neardocs(x).flatMap {
+    case Right(response) =>
+      val ids = response.message.map(_._1.pipe(UUID.fromString))
+      println(f"Searching ids: $ids")
+      db.getDocumentsByIds(ids).attempt.map {
+        case Right(docs) => Right(docs)
+        case Left(err)   => Left(err.getMessage) // Map the error to a string
+      }
 
-      case Left(df: io.circe.Error) =>
-        IO.pure(Left("Parsing error: " + df.getMessage))
-      case Left(other) => IO.pure(Left(other.toString))
-    }
+    case Left(df: io.circe.Error) =>
+      IO.pure(Left("Parsing error: " + df.getMessage))
+    case Left(other) => IO.pure(Left(other.toString))
   }
 }
 def createDoc(
     document: Document
 )(implicit db: DocDB[IO]): IO[Either[String, Unit]] = {
-  db.addDocument(document).attempt.map {
-    case Right(_)  => Right(())
-    case Left(err) => Left(err.getMessage)
+  println("Creating doc... ")
+  (
+    db.addDocument(document),
+    DocumentApiClient.write(document.uid.toString, document.content)
+  ).parTupled.map {
+    case (_, Right(_)) => Right(())
+    case (_, Left(e)) => Left(e.toString)
   }
 }
