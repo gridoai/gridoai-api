bf8e40eee92d823cbe0e5ee99b454877824f2b62
davisuga <daviciencia1@gmail.com>
2023-06-10 14:47:16 -0300

    Total character count util

---
+++ src/main/scala/com/programandonocosmos/utils/List.scala
@@ -0,0 +1,15 @@
+package com.programandonocosmos.utils
+
+def keepTotalWordsUnderN(
+    list: List[(String, Float)],
+    N: Int
+): List[(String, Float)] =
+  list
+    .foldLeft((0, List.empty[(String, Float)])) {
+      case ((accTotal, accList), (word, score)) =>
+        val newTotal = accTotal + word.length
+        if (newTotal <= N) (newTotal, (word, score) :: accList)
+        else (accTotal, accList)
+    }
+    ._2
+    .reverse
