1133aa84d0545fe51bcd2dbbac8159b06feee9a4
davisuga <daviciencia1@gmail.com>
2023-06-10 14:54:15 -0300

    Fixed error reporting and optimized file upload

--- src/main/scala/com/programandonocosmos/endpoints/Http4sFileUpload.scala
+++ src/main/scala/com/programandonocosmos/endpoints/Http4sFileUpload.scala
@@ -2,9 +2,13 @@ package com.programandonocosmos.endpoints
 
 import cats.effect.*
 import cats.implicits.*
+import com.programandonocosmos.adapters.PdfBoxParser
+import com.programandonocosmos.adapters.PdfParser
 import com.programandonocosmos.domain.Document
 import com.programandonocosmos.models.DocDB
 import com.programandonocosmos.services.doc.createDoc
+import fs2.io.file.Files
+import fs2.io.file.Path
 import org.apache.pdfbox.pdmodel.*
 import org.apache.pdfbox.text.PDFTextStripper
 import org.http4s.*
@@ -14,37 +18,67 @@ import org.http4s.multipart.*
 import java.io.File
 import java.nio.file.Paths
 import java.util.UUID
-import fs2.io.file.{Files, Path}
+import scala.util.Try
 
-def fileUploadEndpoint(implicit db: DocDB[IO]) = HttpRoutes.of[IO] {
-  case req @ POST -> Root / "upload" / filename =>
-    req.decode[Multipart[IO]] { m =>
-      m.parts
-        .traverse_(
-          _.body
-            .through(Files[IO].writeAll(Path("temp.pdf")))
-            .compile
-            .drain
-        )
-        .flatMap { _ =>
-          val doc = PDDocument.load(File("temp.pdf"))
-          val stripper = PDFTextStripper()
-          val text = stripper.getText(doc)
-          doc.close()
-          println("loading doc...")
+enum FileUploadError:
+  case PdfParsingError(m: String)
+  case DocumentCreationError(m: String)
+  case UnknownError(m: String)
 
-          createDoc(
-            Document(
-              UUID.randomUUID(),
-              filename,
-              text,
-              f"$filename.pdf",
-              text.length
-            )
-          ).attempt.flatMap {
-            case Right(_) => Ok("Doc created!")
-            case Left(e)  => BadRequest(e.getMessage)
-          }
-        }
-    }
-}
+import FileUploadError._
+
+def extractBytes(m: Multipart[IO]): IO[Array[Byte]] =
+  m.parts
+    .traverse(_.body.compile.to(Array))
+    .map(_.flatten.toArray)
+
+def loadAndExtractText(
+    byteArray: Array[Byte]
+): IO[Either[PdfParsingError, String]] =
+  PdfBoxParser
+    .load(byteArray)
+    .flatMap(doc => PdfBoxParser.getText(doc).map(text => (doc, text)))
+    .flatMap((doc, text) => PdfBoxParser.close(doc).map(_ => text))
+    .attempt
+    .map(_.left.map(t => PdfParsingError(t.getMessage)))
+
+def createAndStoreDocument(filename: String, text: String)(implicit
+    db: DocDB[IO]
+): IO[Either[DocumentCreationError, Unit]] =
+  createDoc(
+    Document(
+      UUID.randomUUID(),
+      filename,
+      text,
+      filename,
+      text.length
+    )
+  ).map:
+    case Right(_) => Right(())
+    case Left(e)  => Left(DocumentCreationError(e))
+
+def mapToResponse(result: Either[FileUploadError, Unit]): IO[Response[IO]] =
+  result match
+    case Right(_) => Ok("Doc created!")
+    case Left(PdfParsingError(e)) =>
+      println("Error parsing PDF file: " + e)
+      BadRequest("An error occurred while parsing the PDF file.")
+    case Left(DocumentCreationError(e)) =>
+      println("Error creating the document: " + e)
+      BadRequest("An error occurred while creating the document.")
+    case Left(e) =>
+      println("Unknown error: " + e)
+      InternalServerError(
+        "An unknown error occurred while processing the request."
+      )
+
+def fileUploadEndpoint(implicit db: DocDB[IO]) =
+  HttpRoutes.of[IO]:
+    case req @ POST -> Root / "upload" / filename =>
+      req.decode[Multipart[IO]]: m =>
+        extractBytes(m)
+          .flatMap(loadAndExtractText)
+          .flatMap:
+            case Right(text) => createAndStoreDocument(filename, text)
+            case Left(error) => IO.pure(Left(error))
+          .flatMap(mapToResponse)
