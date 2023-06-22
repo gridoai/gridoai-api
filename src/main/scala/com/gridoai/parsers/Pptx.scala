ad5d41b89dce4fedbfc0e24378cf94d6d1cd07fc
davisuga <daviciencia1@gmail.com>
2023-06-21 21:54:44 -0300

    replaced deprecated method

--- src/main/scala/com/gridoai/parsers/Pptx.scala
+++ src/main/scala/com/gridoai/parsers/Pptx.scala
@@ -11,7 +11,7 @@ import java.io.File
 import java.io.FileInputStream
 import scala.util.Try
 
-import collection.JavaConverters.asScalaBufferConverter
+import scala.jdk.CollectionConverters.*
 
 def parsePptx(content: Array[Byte]): Try[String] = Try:
   println("Parsing pptx")
