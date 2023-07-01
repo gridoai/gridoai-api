a0189884161b1bd8d13046476bbf1186a27963b3
davisuga <daviciencia1@gmail.com>
2023-07-01 12:24:26 -0300

    Updated docDb trait to  receive auth info

--- src/main/scala/com/gridoai/models/DocDB.scala
+++ src/main/scala/com/gridoai/models/DocDB.scala
@@ -6,9 +6,19 @@ import com.gridoai.domain.DocumentWithEmbedding
 import com.gridoai.domain.SimilarDocument
 
 trait DocDB[F[_]]:
-  def addDocument(doc: DocumentWithEmbedding): F[Either[String, Unit]]
+  def addDocument(
+      doc: DocumentWithEmbedding,
+      orgId: String,
+      role: String
+  ): F[Either[String, Unit]]
   def getNearDocuments(
       embedding: Embedding,
-      limit: Int
+      limit: Int,
+      orgId: String,
+      role: String
   ): F[Either[String, List[SimilarDocument]]]
-  def deleteDocument(uid: UID): F[Either[String, Unit]]
+  def deleteDocument(
+      uid: UID,
+      orgId: String,
+      role: String
+  ): F[Either[String, Unit]]
