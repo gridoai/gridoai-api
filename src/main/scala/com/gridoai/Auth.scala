88d1ee22c920d19d4e2304540f49537b9845de84
davisuga <daviciencia1@gmail.com>
2023-07-02 15:24:06 -0300

    Added auth guards

--- src/main/scala/com/gridoai/Auth.scala
+++ src/main/scala/com/gridoai/Auth.scala
@@ -1,7 +1,12 @@
 package com.gridoai.auth
 import io.circe._
-import io.circe.generic.semiauto._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import java.time.Clock
+import pdi.jwt.{Jwt, JwtAlgorithm}
+import com.gridoai.utils.trace
 
+implicit val clock: Clock = Clock.systemUTC
 case class JWTPayload(
     azp: String,
     iss: String,
@@ -11,7 +16,36 @@ case class JWTPayload(
     sub: String
 )
 
-val mockedJwt = JWTPayload("", "", Some("Incredible org"), None, "", "")
+val mockedJwt =
+  JWTPayload(
+    "www.grioai.com",
+    "accounts.mock.com",
+    Some("org1"),
+    Some("member"),
+    "session_blablablabl",
+    "user_blablablabla"
+  )
+
+def makeMockedToken =
+  Jwt
+    .encode(
+      mockedJwt.asJson.toString,
+      sys.env.get("JWT_SECRET").getOrElse(("")),
+      JwtAlgorithm.HS256
+    )
+    .trace
 
 val getOrgIdAndRolesFromJwt = (jwt: JWTPayload) =>
   (jwt.orgId.getOrElse(jwt.sid), jwt.role.getOrElse("member"))
+
+def limitRole[A](role: Option[String], error: A)(resource: A) =
+  role match
+    case Some("admin") => resource
+    case _             => error
+
+def authErrorMsg(author: Option[String] = Some("User")) =
+  author match
+    case Some(author) =>
+      s"${author} isn't authorized to perform this action"
+    case None =>
+      "User isn't authorized to perform this action"
