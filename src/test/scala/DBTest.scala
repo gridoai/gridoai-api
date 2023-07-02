5aaec6740216411e293b863d6fd8dcbf9897b07e
davisuga <daviciencia1@gmail.com>
2023-07-02 19:48:08 -0300

    Fixed tests

--- src/test/scala/DBTest.scala
+++ src/test/scala/DBTest.scala
@@ -3,20 +3,23 @@ import munit.CatsEffectSuite
 import doobie.implicits._
 import com.gridoai.domain._
 import com.gridoai.utils._
-
+import cats.implicits._
 import java.util.UUID
+import com.gridoai.models.DocDB
+import doobie.util.fragment.Fragment
+import com.gridoai.mock
 
-class DocumentServiceTest extends CatsEffectSuite {
+class DocumentModel extends CatsEffectSuite {
   // Create a test transactor
-  import com.gridoai.models.{xa, PostgresClient}
-
+  import com.gridoai.models.{xa, PostgresClient, POSTGRES_SCHEMA}
+  val DocsDB: DocDB[IO] = PostgresClient
   // Create a test schema if it doesn't exist
   val createSchema =
-    sql"CREATE SCHEMA IF NOT EXISTS test".update.run.transact(xa)
-
+    sql"CREATE SCHEMA IF NOT EXISTS ${Fragment.const(POSTGRES_SCHEMA)}".update.run
+      .transact(xa)
   // Create a test table if it doesn't exist
   val createTable = sql"""
-    CREATE TABLE IF NOT EXISTS test.documents (
+    CREATE TABLE IF NOT EXISTS ${Fragment.const(POSTGRES_SCHEMA)}.documents (
       uid uuid not null default uuid_generate_v4 (),
       name text not null,
       source text not null,
@@ -33,46 +36,55 @@ class DocumentServiceTest extends CatsEffectSuite {
   override def beforeAll(): Unit = {
     (createSchema >> createTable) |> (_.unsafeRunSync()) |> println
   }
+  val doc1Id = UUID.randomUUID()
+  val doc2Id = UUID.randomUUID()
+  val mockEmbedding = List.range(1, 769).map(_.toFloat)
 
   val doc = DocumentWithEmbedding(
-    Document(
-      UUID.randomUUID(), // Fill in with appropriate data
-      "name",
-      "source",
-      "The sky is blue",
-      10
-    ), // Fill in with appropriate data
+    mock.mockedDoc.document.copy(uid = doc1Id),
+    mockEmbedding
+  )
+  val doc2 = DocumentWithEmbedding(
+    mock.mockedDoc.document.copy(uid = doc2Id),
+    mockEmbedding
+  )
 
-    List.range(1, 769).map(_.toFloat)
-    // Fill in with appropriate data
-  ) // Fill in with appropriate data
-  test("addDocument should insert a document into the database") {
-    // Arrange
+  test("Add a document") {
+    val results = List(
+      DocsDB.addDocument(doc, "org1", "admin"),
+      DocsDB.addDocument(doc2, "org2", "admin")
+    ).parSequence
 
-    // Act
-    val result = PostgresClient.addDocument(doc, "org1", "role1")
-
-    // Assert
-    assertIO(result, Right(()))
+    assertIO(results, List(Right(()), Right(())))
   }
 
-  test("getNearDocuments should return documents near the given embedding") {
-    // Arrange
-    val embedding = doc.embedding // Fill in with appropriate data
+  test("Get near documents") {
+    for
+      maybeDocs <-
+        DocsDB.getNearDocuments(doc.embedding, 10, "org1", "member")
+      _ <- IO.println(maybeDocs)
+      _ = assert(maybeDocs.isRight)
+      docs = maybeDocs.getOrElse(List.empty)
+      _ = assert(
+        docs
+          .forall(_.document.uid.toString() != doc2Id.toString())
+      )
+      _ = assert(docs.length > 0)
+    yield ()
+  }
+  test("Deletes a document") {
+    val deletionAssertions = List(
+      ((doc1Id, "org2", "admin"), Left("No document was deleted")),
+      ((doc2Id, "org2", "admin"), Right(())),
+      ((doc1Id, "org1", "admin"), Right(()))
+    )
+    deletionAssertions
+      .map:
+        case ((docId, orgId, role), expected) =>
+          val result = DocsDB.deleteDocument(docId, orgId, role)
+          assertIO(result, expected, s"Failed for $docId, $orgId, $role")
+      .sequence
 
-    // Act
-    val result =
-      PostgresClient.getNearDocuments(embedding, 10, "org1", "role1")
-
-    // Assert
-    assertIO(
-      result.map(_.isRight),
-      true
-    ) // Check that the result is a Right value
-    assertIO(
-      result.map(_.getOrElse(List.empty).length),
-      1
-    ) // Check that the result contains the document we inserted
   }
 
 }
