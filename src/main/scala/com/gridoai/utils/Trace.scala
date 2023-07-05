11ea16ce424b2e19f7103333f5d027f50eb96034
Pedro Cleto <cl3t0.sh@gmail.com>
2023-07-05 09:37:14 -0300

    fix: trace extension and prompt logging

--- src/main/scala/com/gridoai/utils/Trace.scala
+++ src/main/scala/com/gridoai/utils/Trace.scala
@@ -1,6 +1,8 @@
 package com.gridoai.utils
 
 import cats.effect.IO
+import cats.syntax.functor._
+import cats.syntax.applicativeError._
 
 extension [T](a: T)
   def trace =
@@ -8,6 +10,5 @@ extension [T](a: T)
     a
 
 extension [T](a: IO[T])
-  def trace(msg: String = "") =
-    a.attempt.map(println)
-    a
+  def trace(msg: String = ""): IO[T] =
+    a.attempt.flatTap(attempt => IO(println(s"$msg: $attempt"))).rethrow
