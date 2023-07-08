49bbcccdbf8850b5625855a60826958bb9f2ce55
davisuga <daviciencia1@gmail.com>
2023-07-08 10:20:56 -0300

    Updated DocDB trait with new ops

--- src/main/scala/com/gridoai/models/DocDB.scala
+++ src/main/scala/com/gridoai/models/DocDB.scala
@@ -4,21 +4,31 @@ import com.gridoai.domain.UID
 import com.gridoai.domain.Embedding
 import com.gridoai.domain.DocumentWithEmbedding
 import com.gridoai.domain.SimilarDocument
+import com.gridoai.domain.Document
 
 trait DocDB[F[_]]:
   def addDocument(
       doc: DocumentWithEmbedding,
       orgId: String,
       role: String
-  ): F[Either[String, Unit]]
+  ): F[Either[String, String]]
+
   def getNearDocuments(
       embedding: Embedding,
       limit: Int,
       orgId: String,
       role: String
   ): F[Either[String, List[SimilarDocument]]]
+
   def deleteDocument(
       uid: UID,
       orgId: String,
       role: String
   ): F[Either[String, Unit]]
+
+  def listDocuments(
+      orgId: String,
+      role: String,
+      limit: Int,
+      page: Int
+  ): F[Either[String, List[Document]]]
