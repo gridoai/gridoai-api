c9672ca17ae2bca3d0728f8572dd8ea8594bafe4
davisuga <daviciencia1@gmail.com>
2023-07-11 14:24:47 -0300

    Uncoupled PostgresClient with cats-effect IO

--- src/main/scala/com/gridoai/models/Postgres.scala
+++ src/main/scala/com/gridoai/models/Postgres.scala
@@ -31,93 +31,103 @@ val POSTGRES_USER = sys.env.getOrElse("POSTGRES_USER", "postgres")
 val POSTGRES_PASSWORD = sys.env.getOrElse("POSTGRES_PASSWORD", "")
 val POSTGRES_SCHEMA = sys.env.getOrElse("POSTGRES_SCHEMA", "public")
 
-val xa = Transactor.fromDriverManager[IO](
-  "org.postgresql.Driver", // driver classname
-  s"jdbc:postgresql:$POSTGRES_URI", // connect URL (driver-specific)
-  POSTGRES_USER,
-  POSTGRES_PASSWORD
-)
-
 def table(name: String) = Fragment.const(s"$POSTGRES_SCHEMA.${name}")
 val documentsTable = table("documents")
+object PostgresClient {
 
-object PostgresClient extends DocDB[IO]:
+  def apply[F[_]: Async]: DocDB[F] = new DocDB[F] {
+    import cats.implicits._
+    val xa = Transactor.fromDriverManager[F](
+      "org.postgresql.Driver", // driver classname
+      s"jdbc:postgresql:$POSTGRES_URI", // connect URL (driver-specific)
+      POSTGRES_USER,
+      POSTGRES_PASSWORD
+    )
 
-  def addDocument(
-      doc: DocumentWithEmbedding,
-      orgId: String,
-      role: String
-  ): IO[Either[String, String]] =
-    sql"""insert into $documentsTable (uid, name, source, content, token_quantity, embedding, organization, roles) 
-     values (
-      ${doc.document.uid},
-      ${doc.document.name},
-      ${doc.document.source},
-      ${doc.document.content},
-      ${doc.document.tokenQuantity},
-      ${doc.embedding},
-      ${orgId},
-      ${Array(role)}
+    def addDocument(
+        doc: DocumentWithEmbedding,
+        orgId: String,
+        role: String
+    ): F[Either[String, String]] =
+      sql"""insert into $documentsTable (uid, name, source, content, token_quantity, embedding, organization, roles) 
+       values (
+        ${doc.document.uid},
+        ${doc.document.name},
+        ${doc.document.source},
+        ${doc.document.content},
+        ${doc.document.tokenQuantity},
+        ${doc.embedding},
+        ${orgId},
+        ${Array(role)}
+      )""".update.run
+        .transact[F](xa)
+        .map(_ => Right(doc.document.uid.toString())) |> attempt
 
-    )""".update.run
-      .transact(xa)
-      .map(_ => Right(doc.document.uid.toString())) |> attempt
+    def listDocuments(
+        orgId: String,
+        role: String,
+        start: Int,
+        end: Int
+    ): F[Either[String, (List[Document], Int)]] =
+      traceMappable("listDocuments"):
+        sql"""
+       select uid, name, source, content, token_quantity, count(*) over() as total_count 
+       from $documentsTable 
+       where organization = ${orgId} 
+       order by uid asc 
+       limit ${(end - start).abs} 
+       offset ${start}
+     """
+          .query[(Document, Int)]
+          .to[List]
+          .transact[F](xa)
+          .map(results =>
+            val totalCount = results.headOption.map(_._2).getOrElse(0)
+            Right((results.map(_._1), totalCount))
+          ) |> attempt
 
-  def listDocuments(
-      orgId: String,
-      role: String,
-      start: Int,
-      end: Int
-  ): IO[Either[String, List[Document]]] =
-    traceMappable("listDocuments"):
-      sql"select uid, name, source, content, token_quantity from $documentsTable where organization = ${orgId} order by uid asc limit ${(end - start).abs} offset ${start}"
-        .query[Document]
-        .to[List]
-        .transact(xa)
-        .map(Right(_)) |> attempt
-
-  def getNearDocuments(
-      embedding: Embedding,
-      limit: Int,
-      orgId: String,
-      role: String
-  ): IO[Either[String, List[SimilarDocument]]] =
-    traceMappable("getNearDocuments"):
-      println("Getting near docs ")
-      val vector = PGvector(embedding.toArray)
-      val query =
-        sql"select uid, name, source, content, token_quantity, embedding <-> $vector::vector as distance from $documentsTable where organization = $orgId  order by distance asc limit $limit"
-      query
-        .query[Row]
-        .to[List]
-        .transact(xa)
-        .map(
-          _.map(x =>
-            SimilarDocument(
-              document = Document(
-                x.uid,
-                x.name,
-                x.source,
-                x.content,
-                x.token_quantity
-              ),
-              distance = x.distance
+    def getNearDocuments(
+        embedding: Embedding,
+        limit: Int,
+        orgId: String,
+        role: String
+    ): F[Either[String, List[SimilarDocument]]] =
+      traceMappable("getNearDocuments"):
+        println("Getting near docs ")
+        val vector = PGvector(embedding.toArray)
+        val query =
+          sql"select uid, name, source, content, token_quantity, embedding <-> $vector::vector as distance from $documentsTable where organization = $orgId  order by distance asc limit $limit"
+        query
+          .query[Row]
+          .to[List]
+          .transact[F](xa)
+          .map(
+            _.map(x =>
+              SimilarDocument(
+                document = Document(
+                  x.uid,
+                  x.name,
+                  x.source,
+                  x.content,
+                  x.token_quantity
+                ),
+                distance = x.distance
+              )
             )
           )
-        )
-        .map((Right(_))) |> attempt
-
-  def deleteDocument(
-      uid: UID,
-      orgId: String,
-      role: String
-  ): IO[Either[String, Unit]] =
-    sql"delete from $documentsTable where uid = $uid and organization = ${orgId}".update.run
-      .transact(xa)
-      .flatMap(r =>
-        if (r > 0)
-          IO.pure(Right(()))
-        else
-          IO.pure(Left("No document was deleted"))
-      )
-      |> attempt
+          .map((Right(_))) |> attempt
+    def deleteDocument(
+        uid: UID,
+        orgId: String,
+        role: String
+    ): F[Either[String, Unit]] =
+      sql"delete from $documentsTable where uid = $uid and organization = ${orgId}".update.run
+        .transact(xa)
+        .flatMap(r =>
+          if (r > 0)
+            (Right(())).pure[F]
+          else
+            (Left("No document was deleted")).pure[F]
+        ) |> attempt
+  }
+}
