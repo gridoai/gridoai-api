1bf0fdf5493b1400a3779d14f12ba7129c655159
Pedro Cleto <cl3t0.sh@gmail.com>
2023-07-18 15:27:10 -0300

    fix: adjust search endpoint

--- src/main/scala/com/gridoai/mock/Mocks.scala
+++ src/main/scala/com/gridoai/mock/Mocks.scala
@@ -24,6 +24,5 @@ val mockedDocument = Document(
   uid = UUID.fromString("694b8567-8c93-45c6-8051-34be4337e740"),
   name = "Sky observations",
   source = "https://www.nasa.gov/planetarydefense/faq/asteroid",
-  content = "The sky is blue",
-  tokenQuantity = 4
+  content = "The sky is blue"
 )
