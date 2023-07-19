ac94ac434e71c1795bdd24423e983eb124080b5c
davisuga <daviciencia1@gmail.com>
2023-07-19 14:34:02 -0300

    update mocked embeddings api

--- src/main/scala/com/gridoai/adapters/embeddingApi/Mocked.scala
+++ src/main/scala/com/gridoai/adapters/embeddingApi/Mocked.scala
@@ -2,14 +2,16 @@ package com.gridoai.adapters.embeddingApi
 
 import com.gridoai.domain.*
 import cats.effect.IO
+import cats.implicits.*
 
 object Mocked extends EmbeddingAPI[IO]:
   private val mockResponse = Embedding(
-    vector = List.range(0, 768).map(_.toFloat),
+    vector = List.range(0, 767).map(_.toFloat),
     model = EmbeddingModel.Mocked
   )
 
   def embed(
       text: String
-  ): IO[Either[String, Embedding]] =
+  ) =
     IO.pure(Right(mockResponse))
+  def embedMany(text: List[String]) = IO.pure(Right(List(mockResponse)))
