762178d5c3defa4d4a853b4176f729a72370ab4e
davisuga <daviciencia1@gmail.com>
2023-07-20 11:02:59 -0300

    Added retry logic to http client

--- src/main/scala/com/gridoai/adapters/HttpClient.scala
+++ src/main/scala/com/gridoai/adapters/HttpClient.scala
@@ -3,6 +3,8 @@ import cats.effect.IO
 import sttp.client3._
 import sttp.client3.httpclient.cats.HttpClientCatsBackend
 import java.net.InetAddress
+import scala.concurrent.duration.Duration
+import concurrent.duration.DurationInt
 
 val catsBackend =
   HttpClientCatsBackend
@@ -12,8 +14,35 @@ val catsBackend =
 def sendRequest[T] = (r: Request[T, Any]) => catsBackend.flatMap(r.send)
 
 extension [T](r: Request[T, Any])
-  def sendReq() =
-    sendRequest(r)
+  def sendReq(
+      retries: Int = 1,
+      retryDelay: Duration = 0.seconds
+  ): IO[Response[T]] =
+
+    def retry(delay: Duration = retryDelay) =
+      IO.println(s"Retrying request: ${r.uri}")
+        >> IO.sleep(delay)
+        >> sendReq(
+          retries - 1
+        )
+
+    if retries > 0 then
+      sendRequest(r)
+        .flatMap(response => {
+          response
+            .header("Retry-After")
+            .flatMap(_.toIntOption)
+            .map(_.seconds) match
+            case Some(delay) =>
+              println(s"Retrying request: ${r.uri}")
+              retry(delay)
+            case None => IO.pure(response)
+        })
+        .recoverWith:
+          case e =>
+            println(s"Retrying request: ${r.uri}")
+            retry()
+    else sendRequest(r)
 
 def isHostReachable(
     host: String
