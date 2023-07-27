5c32a9efa598f2bd4505f8c2e544e3815abedd79
davisuga <daviciencia1@gmail.com>
2023-07-27 11:54:43 -0300

    Fixed gridoai-ml interface

--- src/main/scala/com/gridoai/adapters/embeddingApi/GridoAIML.scala
+++ src/main/scala/com/gridoai/adapters/embeddingApi/GridoAIML.scala
@@ -20,23 +20,24 @@ case class MessageResponse[T](message: T)
 object GridoAIML extends EmbeddingAPI[IO]:
   val Http = HttpClient(embeddingApiEndpoint)
 
-  def embedMany(text: List[String]): IO[Either[String, List[Embedding]]] =
-    text.map(embed).sequence.map(_.sequence)
+  def embedMany(texts: List[String]): IO[Either[String, List[Embedding]]] =
+    Http
+      .post(f"/embed")
+      .headers(Map("Content-Type" -> "application/json"))
+      .body(Map("texts" -> texts).asJson.toString)
+      .sendReq()
+      .map(
+        _.body.flatMap(
+          decode[MessageResponse[List[List[Float]]]](_).left.map(_.getMessage())
+        )
+      )
+      .mapRight(
+        _.message.map(vec =>
+          Embedding(vector = vec, model = EmbeddingModel.InstructorLarge)
+        )
+      ) |> attempt
 
   def embed(
       text: String
   ): IO[Either[String, Embedding]] = traceMappable("embed"):
-    println(s"Calculating embedding for: ${text.slice(0, 20)}")
-    Http
-      .post(f"/embed")
-      .headers(Map("Content-Type" -> "application/json"))
-      .body(Map("text" -> text).asJson.toString)
-      .sendReq()
-      .map(
-        _.body.flatMap(
-          decode[MessageResponse[List[Float]]](_).left.map(_.getMessage())
-        )
-      )
-      .mapRight(x =>
-        Embedding(vector = x.message, model = EmbeddingModel.InstructorLarge)
-      ) |> attempt
+    embedMany(List(text)).map(_.map(_.head))
