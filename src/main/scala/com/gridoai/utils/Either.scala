ff8e418b3553a3e442c504fc702a5466041097ab
davisuga <daviciencia1@gmail.com>
2023-07-27 11:54:17 -0300

    Monadic fallback

--- src/main/scala/com/gridoai/utils/Either.scala
+++ src/main/scala/com/gridoai/utils/Either.scala
@@ -1,4 +1,6 @@
 package com.gridoai.utils
+import cats.implicits.*
+import cats.Monad
 
 extension [E, T](e: Either[E, T])
   def addLocationToLeft(implicit
@@ -6,3 +8,11 @@ extension [E, T](e: Either[E, T])
       file: sourcecode.File
   ) =
     e.left.map(x => s"${file.value}:${line.value} $x")
+
+def fallbackEitherM[I, O, E, F[_]: Monad](
+    f1: I => F[Either[E, O]],
+    f2: I => F[Either[E, O]]
+)(i: I) =
+  f1(i).flatMap:
+    case Left(_) => f2(i)
+    case x       => x.pure[F]
