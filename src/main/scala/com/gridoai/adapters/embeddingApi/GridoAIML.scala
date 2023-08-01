bcad2227ee1b01b03c572743ca36caabd2818f90
Pedro Cleto <cl3t0.sh@gmail.com>
2023-08-01 14:11:37 -0300

    feat: change embeddingapi trait

--- src/main/scala/com/gridoai/adapters/embeddingApi/GridoAIML.scala
+++ src/main/scala/com/gridoai/adapters/embeddingApi/GridoAIML.scala
@@ -20,6 +20,12 @@ case class MessageResponse[T](message: T)
 object GridoAIML extends EmbeddingAPI[IO]:
   val Http = HttpClient(embeddingApiEndpoint)
 
+  def embedChat(text: String): IO[Either[String, Embedding]] =
+    embed(text)
+
+  def embedChunks(chunks: List[Chunk]): IO[Either[String, List[Embedding]]] =
+    embedMany(chunks.map(_.content))
+
   def embedMany(texts: List[String]): IO[Either[String, List[Embedding]]] =
     Http
       .post(f"/embed")
