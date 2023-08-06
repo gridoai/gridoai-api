4938437a6f4e56a29d4066109c30c617d4813280
Pedro Cleto <cl3t0.sh@gmail.com>
2023-08-06 00:53:16 -0300

    feat: only storing wip of gdrive import

--- src/main/scala/com/gridoai/services/doc/GDrive.scala
+++ src/main/scala/com/gridoai/services/doc/GDrive.scala
@@ -3,16 +3,32 @@ package com.gridoai.services.doc
 import com.gridoai.domain.*
 import com.gridoai.auth.*
 import com.gridoai.models.*
+import com.gridoai.utils.*
 import com.gridoai.adapters.fileStorage.*
+import com.gridoai.adapters.GoogleClient
 import cats.effect.IO
 import cats.implicits.*
 
 def importGDriveDocuments(auth: AuthData)(
     payload: gdriveImportPayload
 )(using db: DocDB[IO]): IO[Either[String, List[String]]] =
-  limitRole(
-    auth.role,
-    Left(authErrorMsg(Some(auth.role))).pure[IO]
-  ):
-    // TODO: Implement gdrive code exchange and documents download
-    Right(List("Hi!")).pure[IO]
+  // limitRole(
+  //   auth.role,
+  //   Left(authErrorMsg(Some(auth.role))).pure[IO]
+  // ):
+  println("importing data from gdrive...")
+  GoogleClient
+    .exchangeCodeForTokens(
+      payload.code,
+      "http://localhost:8080/import/gdrive",
+      List("https://www.googleapis.com/auth/drive.readonly")
+    )
+    .pure[IO]
+    .flatMapRight: (accessToken, _) =>
+      val gdriveClient = getFileStorage("gdrive")(accessToken)
+      payload.paths
+        .traverse(path => gdriveClient.listContent(path))
+        .map(partitionEithers)
+        .mapLeft(_.mkString(","))
+        .trace
+        .mapRight(_ => List("Hi!"))
