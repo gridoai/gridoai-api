8e793f95cee8b4f1e22807b623e54e0d402f6a84
davisuga <daviciencia1@gmail.com>
2023-08-30 10:56:02 -0300

    handling basic events

--- src/main/scala/com/gridoai/adapters/payments/Stripe.scala
+++ src/main/scala/com/gridoai/adapters/payments/Stripe.scala
@@ -9,6 +9,13 @@ import cats.effect.kernel.Sync
 import cats.implicits.*
 import com.stripe.model.Customer
 import com.gridoai.utils.attempt
+import com.stripe.net.ApiResource
+import com.stripe.model.Event
+import com.stripe.model.PaymentIntent
+import com.stripe.model.PaymentMethod
+import com.stripe.net.Webhook
+import com.gridoai.adapters.ClerkClient
+import com.stripe.model.Subscription
 
 val client = com.stripe.StripeClient(
   sys.env
@@ -47,6 +54,49 @@ def handleCreateCostumer[F[_]](
     case _ =>
       "Unauthorized".asLeft.pure[F]
 
-def handlePayment(event: String) =
-  // turns into strpe pay
-  ???
+def handleDeletionEvent(orgId: String) =
+  ClerkClient.deleteOrg(orgId)
+
+def handleEvent[F[_]](
+    eventRaw: String,
+    sigHeader: String
+)(using F: Sync[F]): F[Either[String, String]] = Sync[F].blocking {
+  println(s"Event from stripe: ${eventRaw}")
+
+  val event = Webhook.constructEvent(
+    eventRaw,
+    sigHeader,
+    sys.env
+      .get("WEBHOOK_KEY")
+      .getOrElse(throw new Exception("WEBHOOK_KEY not found"))
+  )
+
+  // Deserialize the nested object inside the event
+  val dataObjectDeserializer = event.getDataObjectDeserializer
+  val stripeObject = dataObjectDeserializer.getObject.get
+
+  // Move this to separate case if we add other types of events
+  val subscription = stripeObject.asInstanceOf[Subscription]
+  val eventType = event.getType
+  println(s"Event type: ${eventType}")
+  // Handle the event
+  eventType match
+
+    case "customer.subscription.created" =>
+      subscription.getItems().getData().get(0)
+      "???"
+    case "customer.subscription.deleted" =>
+      "???"
+    case "customer.subscription.updated" =>
+      "???"
+    // Then define and call a method to handle the successful attachment of a PaymentMethod.
+    // handlePaymentMethodAttached(paymentMethod);
+
+    // ... handle other event types
+    case _ =>
+      System.out.println("Unhandled event type: " + event.getType)
+      "???"
+
+  // val event = ApiResource.GSON.fromJson(eventRaw, classOf[Event])
+  // val dataObjectDeserializer = event.getDataObjectDeserializer()
+}.attempt |> attempt
