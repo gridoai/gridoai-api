40f37006c7f98a439e1575f52119eabfcabd47b7
davisuga <daviciencia1@gmail.com>
2023-11-27 19:20:44 -0300

    Refactor GDriveClient to handle additional file types

--- src/main/scala/com/gridoai/adapters/fileStorage/GDriveClient.scala
+++ src/main/scala/com/gridoai/adapters/fileStorage/GDriveClient.scala
@@ -21,7 +21,8 @@ object GDriveClient:
       Some(
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
       )
-    case "application/vnd.google-apps.sheet" =>
+    case "application/vnd.google-apps.sheet" |
+        "application/vnd.google-apps.spreadsheet" =>
       Some("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     case _ => None
 
@@ -38,8 +39,7 @@ object GDriveClient:
           .mkString(" or ")
         logger.info(s"query: $query")
         Sync[IO].blocking(
-          driveService
-            .files()
+          driveService.files
             .list()
             .setQ(query)
             .execute()
@@ -56,18 +56,19 @@ object GDriveClient:
         Sync[IO].blocking(
           files
             .map(file =>
+              logger.info(
+                s"Downloading file: ${file.name} with mime ${file.mimeType}"
+              )
               val outputStream = ByteArrayOutputStream()
               mapMimeTypes(file.mimeType) match
                 case Some(mimeType) =>
                   logger.info("Exporting file from Google Drive...")
-                  driveService
-                    .files()
+                  driveService.files
                     .`export`(file.id, mimeType)
                     .executeMediaAndDownloadTo(outputStream)
                 case None =>
                   logger.info("Downloading file from Google Drive...")
-                  driveService
-                    .files()
+                  driveService.files
                     .get(file.id)
                     .executeMediaAndDownloadTo(outputStream)
               File(meta = file, content = outputStream.toByteArray)
@@ -78,7 +79,7 @@ object GDriveClient:
       def isFolder(fileId: String): IO[Either[String, Boolean]] =
         (Sync[IO].blocking:
           val file =
-            driveService.files().get(fileId).setFields("mimeType").execute()
+            driveService.files.get(fileId).setFields("mimeType").execute()
           Right(file.getMimeType == "application/vnd.google-apps.folder")
         ) |> attempt
 
@@ -86,7 +87,7 @@ object GDriveClient:
         Sync[IO].blocking(
           fileIds
             .map(fileId =>
-              val file = driveService.files().get(fileId).execute()
+              val file = driveService.files.get(fileId).execute()
               FileMeta(file.getId, file.getName, file.getMimeType)
             )
             .asRight
