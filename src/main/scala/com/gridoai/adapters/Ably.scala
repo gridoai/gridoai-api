42f7aa482683f6f1e1cd7fc00631909328152944
davisuga <daviciencia1@gmail.com>
2023-12-23 13:25:49 -0300

    Add AblyNotificationService and generateToken functions

---
+++ src/main/scala/com/gridoai/adapters/Ably.scala
@@ -0,0 +1,68 @@
+package com.gridoai.adapters.notifications
+import cats.effect.IO
+
+import io.ably.lib.realtime.AblyRealtime
+import io.ably.lib.realtime.Channel
+import scala.concurrent.Future
+import scala.util.{Try, Success, Failure}
+import cats.effect.kernel.Async
+import cats.effect.kernel.Sync
+import io.ably.lib.rest.Auth.TokenParams
+import io.ably.lib.rest.Auth.AuthOptions
+import io.ably.lib.rest.AblyRest
+import io.ably.lib.realtime.CompletionListener
+import io.ably.lib.types.ErrorInfo
+val ablyClient = AblyRest(sys.env("ABLY_KEY"))
+
+class AblyNotificationService[F[_]: Async]()
+    extends NotificationService[F]
+    with UploadNotificationService[F]:
+  val logger = org.slf4j.LoggerFactory.getLogger(
+    getClass()
+  )
+  override def notifyUpload(
+      status: UploadStatus,
+      user: String
+  ): F[Either[String, Unit]] =
+    val topic = s"$user:upload"
+    val channel = s"$user:upload-status"
+    val content = status.toString()
+    sendNotification(topic, channel, content)
+
+  def sendNotification(
+      topic: String,
+      channelName: String,
+      content: String
+  ): F[Either[String, Unit]] =
+    val channel = ablyClient.channels.get(channelName)
+    Async[F].async_ : callback =>
+      try
+        channel.publishAsync(
+          topic,
+          content,
+          new CompletionListener():
+            override def onSuccess(): Unit =
+              logger.info(s"Notification sent to $channelName")
+              callback(Right(Right(())))
+
+            override def onError(reason: ErrorInfo): Unit =
+              logger.error(
+                s"Notification failed to send to $channelName: ${reason.message}"
+              )
+              callback(Left(new Exception(reason.message)))
+        )
+      catch case ex => Left(ex.getMessage)
+
+def generateToken[F[_]: Sync](clientId: String) =
+  Sync[F].blocking:
+    try
+      val tokenParams = TokenParams()
+      tokenParams.clientId = clientId
+      tokenParams.capability = s"""{"$clientId:*":["publish","subscribe"]}"""
+      val authOptions = AuthOptions()
+      val tokenRequest =
+        ablyClient.auth.requestToken(tokenParams, null)
+      val token = tokenRequest.token
+
+      Right(token)
+    catch case ex => Left(ex.getMessage)
