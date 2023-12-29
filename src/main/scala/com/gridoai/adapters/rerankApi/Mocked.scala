74ea75fb9c2b3d206c4941cc25cbcc2595f60c84
davisuga <daviciencia1@gmail.com>
2023-12-28 23:02:12 -0300

    Add load test configuration file and mock notification and rerank services

--- src/main/scala/com/gridoai/adapters/rerankApi/Mocked.scala
+++ src/main/scala/com/gridoai/adapters/rerankApi/Mocked.scala
@@ -9,11 +9,9 @@ import com.gridoai.domain.RelevantChunk
 import cats.Id
 import cats.effect.kernel.Sync
 
-// Assuming the definitions of UID, Source, Chunk, RelevantChunk, RerankPayload are as provided
-
 class MockRerankAPI[F[_]: Sync] extends RerankAPI[F] {
 
-  override def rerank(
+  def rerank(
       payload: RerankPayload
   ): F[Either[String, List[RelevantChunk]]] = Sync[F].blocking {
     Thread.sleep(500)
