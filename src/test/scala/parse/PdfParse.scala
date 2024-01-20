ff3adf5ed10dfc8e24ad862009f0804ecc0b5647
davisuga <daviciencia1@gmail.com>
2024-01-20 15:33:53 -0300

    Fix extractTextFromPdf test assertion

--- src/test/scala/parse/PdfParse.scala
+++ src/test/scala/parse/PdfParse.scala
@@ -3,12 +3,13 @@ import munit.CatsEffectSuite
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import cats.effect.IO
 class PdfParserTest extends CatsEffectSuite {
   test("Pdf parse") {
     val t = Files.readAllBytes(Paths.get("./src/test/resources/test.pdf"))
     val expected =
       Files.readString(Paths.get("./src/test/resources/expected_pdf.txt"))
-    extractTextFromPdf(t).assertEquals(Right(expected))
+    extractTextFromPdf(t).value.assertEquals(Right(expected))
 
   }
 }
